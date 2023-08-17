12a82939741a5349141165bc28b5042baedd602c
ParvinValizade <parvin.valizade@mail.ru>
2023-08-17 21:28:56 +0400

    Consumer group 1

---
+++ ms-stock/src/main/java/com/company/msstock/kafka/OrderConsumer.java
@@ -0,0 +1,20 @@
+package com.company.msstock.kafka;
+
+import com.company.basedomains.event.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event) {
+        LOGGER.info("Order event received in stock service => {}", event.toString());
+
+        // save the order event into the database
+    }
+}
